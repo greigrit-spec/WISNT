8e76ebdc8f2c26327cbdb2ab1e09a52dc1cd1a8a
greigrit-spec <greigrit@gmail.com>
2025-11-19 10:00:57 +0300

    Update WIHER.ps1

--- WIHER.ps1
+++ WIHER.ps1
@@ -4,4 +4,4 @@ $Content = irm -Uri $BatUrl
 $UTF8NoBOM = New-Object System.Text.UTF8Encoding($False)
 [System.IO.File]::WriteAllText($TempPath, $Content, $UTF8NoBOM)
 Start-Process -FilePath cmd -ArgumentList "/k chcp 65001 & $TempPath & exit" -Wait
-Remove-Item $TempPath
+
